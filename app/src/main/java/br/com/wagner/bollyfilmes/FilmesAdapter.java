dc88d6a1ded35175d527dd1b4aa736a5caedd620
Wagner Rodrigues <wagnersr92@gmail.com>
2018-11-21 00:02:23 -0200

    Criação da classe FilmesAdapter que possui o adapter para a exibição da lista de filmes

---
+++ app/src/main/java/br/com/wagner/bollyfilmes/FilmesAdapter.java
@@ -0,0 +1,44 @@
+package br.com.wagner.bollyfilmes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class FilmesAdapter extends ArrayAdapter<ItemFilme> {
+
+    public FilmesAdapter(Context context, ArrayList<ItemFilme> filmes){
+        super(context,0,filmes);
+    }
+
+    @Override
+    public View getView(int position, View convertView,  ViewGroup parent) {
+
+        View itemView = convertView;
+        if(itemView == null){
+            itemView = LayoutInflater.from(getContext()).inflate(R.layout.item_filme, parent, false);
+        }
+
+        ItemFilme filme = getItem(position);
+
+        TextView titulo = itemView.findViewById(R.id.item_titulo);
+        titulo.setText(filme.getTitulo());
+
+        TextView descricao = itemView.findViewById(R.id.item_descricao);
+        descricao.setText(filme.getDescricao());
+
+        TextView dataLancamento = itemView.findViewById(R.id.item_data);
+        dataLancamento.setText(filme.getDataLancamento());
+
+        RatingBar avaliacao = itemView.findViewById(R.id.item_avaliacao);
+        avaliacao.setRating(filme.getAvaliacao());
+
+
+        return super.getView(position, convertView, parent);
+    }
+}
--- app/src/main/java/br/com/wagner/bollyfilmes/ItemFilme.java
+++ app/src/main/java/br/com/wagner/bollyfilmes/ItemFilme.java
@@ -24,7 +24,6 @@ public class ItemFilme {
     }
 
     public String getId() {
-
         return id;
     }
 
